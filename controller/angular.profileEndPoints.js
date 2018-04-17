492f131002d0d0afe13f8ef5d0dbc3562fc0ab7a
Goutham <gouthamcold@gmail.com>
2018-04-17 10:26:11 -0400

    added a route in angularendpoints

--- controller/angular.profileEndPoints.js
+++ controller/angular.profileEndPoints.js
@@ -10,19 +10,21 @@ module.exports = function(app){
             res.send(result);
         });
     });
-
+    
     //this fetches data from accDetails collection
     app.get('userProfile/userAccDetails', (req,res) => {
 
     });
-
+    
     //this fetches data from stkWatchlist collection
     app.get('userProfile/userWatchlistStocks', (req,res)=>{
-
+        stkWathclist.findOne({'googleID':'1234'},function(err,result){
+            res.send(result);
+        });
     });
-
+    
     //this fetches data from stkOwned collection
     app.get('userProfile/userOwnedStocks', (req,res)=>{
-
-    });
-}
+        
+        });
+};
